9e63d37debfbfef57e6b2cbe5c4d9f14f7d7a8ea
Marlon Klagenberg <marlon.klc@gmail.com>
2017-12-23 14:48:24 -0200

    remove caracter

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get('/', function(req, res) {
 });
 
 app.get('/api/v1/:rota', function(req, res) {
-  res.status(200).-json({
+  res.status(200).json({
     params: req.params,
     query: req.query
   });
