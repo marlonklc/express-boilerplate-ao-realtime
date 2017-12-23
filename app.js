431dc4eb3d495bc724c658cf9d261663b9dbcac7
Marlon Klagenberg <marlon.klc@gmail.com>
2017-12-23 14:42:41 -0200

    ident code

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-		res.send('Hello world');
+	res.send('Hello world');
 });
 
 app.get('/api/v1/:rota', function(req, res) {
-		res.status(200).-json({
-				params: req.params,
-				query: req.query
-		});
+  res.status(200).-json({
+    params: req.params,
+		query: req.query
+	});
 });
 
 
