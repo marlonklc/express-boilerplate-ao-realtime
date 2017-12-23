674713fe4aa35e65514934bc09f4adb2b0c2504a
Marlon Klagenberg <marlon.klc@gmail.com>
2017-12-23 14:38:00 -0200

    update files local to remote

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+		res.send('Hello world');
+});
+
+app.get('/api/v1/:rota', function(req, res) {
+		res.status(200).-json({
+				params: req.params,
+				query: req.query
+		});
+});
+
+
+app.listen(8080);
---
+++ app3-socketio.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var http = require('http');
+var app = express();
+var server = http.Server(app);
+var io = require('socket.io')(server);
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+	socket.on('edit', function(data) {
+		socket.broadcast.emit('edit', data);
+	});
+});
+
+server.listen(8080);
